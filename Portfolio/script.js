96961cc1e4465fc9cd813b6f38842414c9dd6686
Thunder <nonthacha.h@gmail.com>
2023-06-29 19:53:33 +0700

    update

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -92,3 +92,79 @@ document.addEventListener('mouseup', handleMouseUp)
 document.addEventListener('touchstart', handleMouseDown)
 document.addEventListener('touchmove', handleMouseMove)
 document.addEventListener('touchend', handleMouseUp)
+
+const stars = document.getElementById('stars');
+const starsCtx = stars.getContext('2d');
+
+// global variables
+let screen, starsElements, starsParams = { speed: 2, number: 300, extinction: 4 };
+
+// run stars
+setupStars();
+
+// update stars on resize to keep them centered
+window.onresize = function() {
+    setupStars();
+};
+
+// star constructor
+function Star() {
+    this.x = Math.random() * stars.width;
+    this.y = Math.random() * stars.height;
+    this.z = Math.random() * stars.width;
+
+    this.move = function() {
+        this.z -= starsParams.speed;
+        if (this.z <= 0) {
+            this.z = stars.width;
+        }
+    };
+
+    this.show = function() {
+        let x, y, rad, opacity;
+        x = (this.x - screen.c[0]) * (stars.width / this.z);
+        x = x + screen.c[0];
+        y = (this.y - screen.c[1]) * (stars.width / this.z);
+        y = y + screen.c[1];
+        rad = stars.width / this.z;
+        opacity = (rad > starsParams.extinction) ? 1.5 * (2 - rad / starsParams.extinction) : 1;
+
+        starsCtx.beginPath();
+        starsCtx.fillStyle = "rgba(255, 255, 255, " + opacity + ")";
+        starsCtx.arc(x, y, rad, 0, Math.PI * 2);
+        starsCtx.fill();
+    }
+}
+
+// setup <canvas>, create all the starts
+function setupStars() {
+    screen = {
+        w: window.innerWidth,
+        h: window.innerHeight,
+        c: [ window.innerWidth * 0.5, window.innerHeight * 0.5 ]
+    };
+    window.cancelAnimationFrame(updateStars);
+    stars.width = screen.w;
+    stars.height = screen.h;
+    starsElements = [];
+    for (let i = 0; i < starsParams.number; i++) {
+        starsElements[i] = new Star();
+    }
+}
+
+
+const gradient = starsCtx.createLinearGradient(1500, 0, 0, 0);
+gradient.addColorStop(0, 'black');
+gradient.addColorStop(1, '#220033');
+// redraw the frame
+function updateStars() {
+  starsCtx.fillStyle = gradient;
+  starsCtx.fillRect(0, 0, stars.width, stars.height);
+  starsElements.forEach(function (s) {
+    s.show();
+    s.move();
+  });
+  window.requestAnimationFrame(updateStars);
+}
+
+updateStars();
