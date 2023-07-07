42de44627299f3e528e66db8e46b6af62cb4a561
Thunder <nonthacha.h@gmail.com>
2023-07-07 18:23:45 +0700

    not finish

--- ParallaxScroll3/app.js
+++ ParallaxScroll3/app.js
@@ -1,45 +1,44 @@
 let MyDiv = document.getElementById('test');
 let btn = document.getElementById('btn');
+let next_1 = document.getElementById('next_1');
+let bk = document.getElementById('back');
 
-// window.addEventListener('scroll',function (){
-//     let value = this.window.scrollLeft;
-//     MyDiv.style.left = value * 10. +'%';
-// })
-
-// window.addEventListener("scroll", function() {
-//     var scrollPosition = window.scrollX;
-//     var parallaxElements = document.getElementsByClassName("parallax-item");
-  
-//     for (var i = 0; i < parallaxElements.length; i++) {
-//       var parallaxElement = parallaxElements[i];
-//       var parallaxAmount = scrollPosition * 1.5; // Adjust the parallax speed here
-  
-//       parallaxElement.style.transform = "translateX(" + parallaxAmount + "px)";
-//     }
-//   });
-
-window.onload = function() {
-
-    window.addEventListener('scroll', function(e) {
-      
-      let s = this.scrollY;
-      let w = this.outerWidth;
-      let h = document.getElementsByClassName('box')[0].clientWidth;
-      let h_b = document.getElementsByClassName('wrapper')[0].clientWidth;
-      let p = s/h*100;
-      let p_b = s/h_b*100;
-      let opas = 1-1/100*p_b;
-      let z_1 = 1 + (w / 10000 * p_b);
-      document.getElementsByClassName('p-item4')[0].style= `transform: scale(${z_1});opacity: ${opas}`;
-      let z_2 = 1+(w/5000000*p);
-      document.getElementsByClassName('p-item1')[0].style= `transform: scale(${z_2})`;
-      let hr = w/2000*p_b;
-      let z_3 = 1+(w*0.000005*p_b);
-      document.getElementsByClassName('p-item2')[0].style= `transform: translate3d(${hr}px,0,0) scale(${z_3})`;
-      let hr_2 = w/1500*p_b;
-      let z_4 = 1+(w*0.00001*p_b);
-      document.getElementsByClassName('p-item3')[0].style= `transform: translate3d(${hr_2}px,0,0) scale(${z_4})`;
-    })
-    
-    
+document.addEventListener('DOMContentLoaded', function() { // check state of back button
+  if (next_1.classList.contains('active')) {
+    bk.style.display = "none";
   }
+});
+
+next_1.addEventListener('click', function() {
+
+  if (next_1.classList.contains('active')) { // page 1 => page 2
+    next_1.classList.remove('active');
+    next_1.classList.add('active2');
+    bk.style.display = "block";
+    location.href = "#second";
+  }else if (next_1.classList.contains('active2')){ //page 2 => page 3
+    next_1.classList.add('active');
+    next_1.classList.remove('active2');
+    bk.style.display = "block";
+    next_1.style.display = "none";
+    location.href = "#third";
+    location.reload();
+  }
+});
+
+bk.addEventListener('click', () => {
+  window.history.back();
+  if(next_1.classList.contains('active')) { //page 1
+    next_1.classList.remove('active');
+    next_1.classList.add('active2');
+    bk.style.display = "none";
+  }
+})
+
+/*
+  forward active = back hide
+  forward active2 = back active
+  forward active3 = back active2
+
+
+*/
